d2c098688a2b0e863e6de9782d9f0baf65555cb0
Cedric-2020 <62846821+Cedric-2020@users.noreply.github.com>
2021-10-22 00:12:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#This block below contains Terraform settings, including the required providers Terraform will use to provision my infra
 terraform {
   required_providers {
     aws = {
@@ -5,17 +6,16 @@ terraform {
       version = "~> 3.27"
     }
   }
-#this block grab providers from terraform registry : here AWS in version 3.27
-  
+
   required_version = ">=0.14.9"
 
 }
-
+#and above this block configure providers from terraform registry : here AWS
 provider "aws" {
   profile = "defaults"
   region  = "us-west-2"
 }
-
+#resource blocks below is here in order to define components of my infrastructure. 
 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
